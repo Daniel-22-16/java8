e97b3fe99877b795409a478910951821b602b776
Daniel-22-16 <1475937@senati.pe>
2023-03-23 11:27:36 -0500

    java8

---
+++ JavaApplication3.java
@@ -0,0 +1,15 @@
+
+package javaapplication3;
+import java.util.Scanner;
+public class JavaApplication3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int contImpar = 0;
+        int contPar = 0;
+        int sumaPar = 0;
+        String resp;
+
+    }
+    
+}
